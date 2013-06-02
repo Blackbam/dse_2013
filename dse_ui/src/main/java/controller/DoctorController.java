08e8785368086ae8a12554e218a823b3af6beb3d
mwagnurr <mwagnurr@gmail.com>
2013-06-02 14:43:27 +0200

    adds messaging listener in dse_messenger
    (able to listen and process incomming messages now)

--- dse_ui/src/main/java/controller/DoctorController.java
+++ dse_ui/src/main/java/controller/DoctorController.java
@@ -56,6 +56,8 @@ public class DoctorController {
 
 		logger.info("trying to reserve patient: " + patientID + ". sending message to allocator");
 
+		//amqpTemplate.convertAndSend("allocator", message.getValue());
+		
 		return "doctor";
 	}
 
